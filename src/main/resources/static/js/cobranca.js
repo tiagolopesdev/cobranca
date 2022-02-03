721fc310b7b6b71a4e7b2c4063f78293946b504b
tiagoLopes <saxtiago14@gmail.com>
2022-02-02 21:56:27 -0300

    Funções done() e fail() para tratamento de erro ao atualizar status do título.

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -1,14 +1,14 @@
 $('#confirmacaoExclusao').on('show.bs.modal', function (event) {
 
     var button = $(event.relatedTarget);
-    
+
     var codigoTitulo = button.data('codigo');
     var descricaoTitulo = button.data('descricao');
-    
+
     var modal = $(this);
     var form = modal.find('form');
     var action = form.data('url-base');
-    
+
     if (!action.endsWith('/')) {
         action += '/';
     }
@@ -19,25 +19,35 @@ $('#confirmacaoExclusao').on('show.bs.modal', function (event) {
 });
 
 $(function () {
-    
+
     $('[rel="tooltip"]').tooltip('show').tooltip('toggle');
-    
+
     $('#valor').maskMoney({
         decimal: ',',
         thousands: '.',
         allowZero: true
-    });    
-    
+    });
+
     $('.js-status').on('click', function (event) {
         event.preventDefault();
-        
+
         var buttonSet = $(event.currentTarget);
-        var urlSet = buttonSet.attr('href');  
-        
+        var urlSet = buttonSet.attr('href');
+
         var response = $.ajax({
             url: urlSet,
             type: 'PUT'
         });
+
+        reponse.done(function (e) {
+            var codigoTitulo = buttonSet.hide();
+        });
+
+        response.fail(function (e) {
+            console.log(e);
+            alert("Erro recebendo cobrança!");
+        });
+
     });
-    
+
 });
