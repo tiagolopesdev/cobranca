27c448c810a79550c723803cb24986eb1f4b4ef7
tiagoLopes <saxtiago14@gmail.com>
2022-03-25 22:07:03 -0300

    Classe de serviço para o repositório de cartão implementada com método salvar.

---
+++ src/main/java/tiagoDev/cobranca/services/CartaoService.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tiagoDev.cobranca.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tiagoDev.cobranca.model.Cartao;
+import tiagoDev.cobranca.repository.CartaoRepository;
+
+/**
+ *
+ * @author tiagolopes
+ */
+@Service
+public class CartaoService {
+
+    @Autowired
+    private CartaoRepository cartaoRepository;
+
+    public void saveCartaoService(Cartao c) {
+        cartaoRepository.save(c);
+    }
+
+}
