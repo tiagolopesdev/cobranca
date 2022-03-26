179615d9f502bd7f63e0f7a99695e7f133839153
tiagoLopes <saxtiago14@gmail.com>
2022-03-25 23:02:43 -0300

    Método, implementado no service, para buscar todos os cartões adicinados.

--- src/main/java/tiagoDev/cobranca/services/CartaoService.java
+++ src/main/java/tiagoDev/cobranca/services/CartaoService.java
@@ -1,9 +1,10 @@
-/*
+ /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package tiagoDev.cobranca.services;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tiagoDev.cobranca.model.Cartao;
@@ -21,6 +22,10 @@ public class CartaoService {
 
     public void saveCartaoService(Cartao c) {
         cartaoRepository.save(c);
+    }
+    
+    public List<Cartao> findAllCartaoService(){
+        return cartaoRepository.findAll();
     }
 
 }
